8a42ae235eaaa19fefd586a044c1699fdf3ae6ae
ChaR <1215585066@qq.com>
2022-07-29 16:08:46 +0800

    feat:1 集成旧版renren-fast,renren-generator; 2 新增gulimall-common,为每一个微服务公共的依赖，bean，工具类等; 3 product模块初始化CRUD,单元测试

--- gulimall-product/src/main/java/com/charlie/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/charlie/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,10 @@
 package com.charlie.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.charlie.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/charlie/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/charlie/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,42 @@
 package com.charlie.gulimall.product;
 
-import org.junit.jupiter.api.Test;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.charlie.gulimall.product.entity.BrandEntity;
+import com.charlie.gulimall.product.service.BrandService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class GulimallProductApplicationTests {
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=GulimallProductApplication.class)
+public class GulimallProductApplicationTests {
+
+    @Autowired
+    BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setDescript("hello world");
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功");
+
+//        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1l));
+        List<BrandEntity> list = brandService.list();
+        list.forEach((item) ->{
+            System.out.println(item);
+        });
+
     }
+
+
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
